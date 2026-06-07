bb192cacd0c291534f9dfe11003ce840f89acc65
Shalev Shushy <shushy245@gmail.com>
2026-06-07 23:38:14 +0300

    chore: port

--- packages/backend/drizzle.config.ts
+++ packages/backend/drizzle.config.ts
@@ -6,7 +6,7 @@ export default defineConfig({
     dialect: 'postgresql',
     dbCredentials: {
         host: process.env['DB_HOST'] ?? 'localhost',
-        port: Number(process.env['DB_PORT'] ?? 5432),
+        port: Number(process.env['DB_PORT'] ?? 5434),
         user: process.env['DB_USER'] ?? 'reading_room',
         password: process.env['DB_PASSWORD'] ?? 'reading_room',
         database: process.env['DB_NAME'] ?? 'reading_room',
